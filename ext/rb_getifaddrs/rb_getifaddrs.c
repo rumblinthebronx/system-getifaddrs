a12546feb5e4927a0420b6a0232a7e004299dc97
rumblinthebronx <sujin.phil@gmail.com>
2012-10-26 12:11:46 -0700

    Fixed formatting and converted tabs to spaces

--- ext/rb_getifaddrs/rb_getifaddrs.c
+++ ext/rb_getifaddrs/rb_getifaddrs.c
@@ -19,58 +19,58 @@ char * get_if_name(struct ifaddrs *ifa){
 }
 
 int get_if_host(struct ifaddrs *ifa, char *host){
-        int family = get_if_family(ifa);
+    int family = get_if_family(ifa);
 	if(getnameinfo(ifa->ifa_addr, 
-                        (family == AF_INET) ? sizeof(struct sockaddr_in) :
-                        sizeof(struct sockaddr_in6),
-			host, NI_MAXHOST,
-			NULL, 0, NI_NUMERICHOST))
+        (family == AF_INET) ? sizeof(struct sockaddr_in) :
+        sizeof(struct sockaddr_in6),
+	    host, NI_MAXHOST,
+		NULL, 0, NI_NUMERICHOST))
 		return 0;
 	return 1;
 }
 
 int get_if_netmask(struct ifaddrs *ifa, char *netmask){
-        int family = get_if_family(ifa);
+    int family = get_if_family(ifa);
 	if(getnameinfo(ifa->ifa_netmask, 
-                        (family == AF_INET) ? sizeof(struct sockaddr_in) :
-                        sizeof(struct sockaddr_in6),
-			netmask, NI_MAXHOST,
-			NULL, 0, NI_NUMERICHOST))
+        (family == AF_INET) ? sizeof(struct sockaddr_in) :
+        sizeof(struct sockaddr_in6),
+		netmask, NI_MAXHOST,
+		NULL, 0, NI_NUMERICHOST))
 		return 0;
 	return 1;
 }
 
 VALUE set_if_hash(VALUE rb_if_hash, struct ifaddrs *ifa, int family){
-        char *if_host, *if_netmask, *if_name;
-        if_name = get_if_name(ifa);
-	if_host = malloc(sizeof(char) * NI_MAXHOST);
-	if (! get_if_host(ifa, if_host))
-		rb_raise(rb_eSystemCallError, "Can't get IP from %s", if_name);
+    char *if_host, *if_netmask, *if_name;
+    if_name = get_if_name(ifa);
+    if_host = malloc(sizeof(char) * NI_MAXHOST);
+    if (! get_if_host(ifa, if_host))
+        rb_raise(rb_eSystemCallError, "Can't get IP from %s", if_name);
 
-	if_netmask = malloc(sizeof(char) * NI_MAXHOST);
-	if (! get_if_netmask(ifa, if_netmask))
-		rb_raise(rb_eSystemCallError, "Can't get IP from %s", if_name);
+    if_netmask = malloc(sizeof(char) * NI_MAXHOST);
+    if (! get_if_netmask(ifa, if_netmask))
+        rb_raise(rb_eSystemCallError, "Can't get IP from %s", if_name);
 
-	char *str_inet_name = "inet_addr_v4";
-	char *str_inet_name6 = "inet_addr_v6";
-	char *str_netmask_name = "netmask_v4";
-	char *str_netmask_name6 = "netmask_v6";
-        VALUE rb_if_data_hash = rb_hash_aref(rb_if_hash, rb_str_intern(rb_str_new2(if_name)));
+    char *str_inet_name = "inet_addr_v4";
+    char *str_inet_name6 = "inet_addr_v6";
+    char *str_netmask_name = "netmask_v4";
+    char *str_netmask_name6 = "netmask_v6";
+    VALUE rb_if_data_hash = rb_hash_aref(rb_if_hash, rb_str_intern(rb_str_new2(if_name)));
 
-	
-        if (rb_if_data_hash == Qnil) {
-		rb_if_data_hash = rb_hash_new();
-		rb_hash_aset(rb_if_hash,
-				rb_str_intern(rb_str_new2(if_name)),
-				rb_if_data_hash);
-       }
-	rb_hash_aset(rb_if_data_hash,
-			rb_str_intern(rb_str_new2((family == AF_INET) ? str_inet_name : str_inet_name6)),
-			rb_str_new2(if_host));
-	rb_hash_aset(rb_if_data_hash,
-			rb_str_intern(rb_str_new2((family == AF_INET) ? str_netmask_name : str_netmask_name6)),
-			rb_str_new2(if_netmask));
-	return rb_if_data_hash;
+    if (rb_if_data_hash == Qnil) {
+        rb_if_data_hash = rb_hash_new();
+        rb_hash_aset(rb_if_hash,
+            rb_str_intern(rb_str_new2(if_name)),
+            rb_if_data_hash);
+    }
+
+    rb_hash_aset(rb_if_data_hash,
+        rb_str_intern(rb_str_new2((family == AF_INET) ? str_inet_name : str_inet_name6)),
+        rb_str_new2(if_host));
+    rb_hash_aset(rb_if_data_hash,
+        rb_str_intern(rb_str_new2((family == AF_INET) ? str_netmask_name : str_netmask_name6)),
+        rb_str_new2(if_netmask));
+    return rb_if_data_hash;
 }
 
 VALUE rb_get_ifaddrs(void)
@@ -90,11 +90,11 @@ VALUE rb_get_ifaddrs(void)
         family = get_if_family(ifa);
         if (family == AF_INET || family == AF_INET6)
         {
-  	      set_if_hash(rb_if_hash, ifa, family);  
+            set_if_hash(rb_if_hash, ifa, family);  
         }
-    }
-    freeifaddrs(ifaddr);
-    return rb_if_hash;
+        }
+        freeifaddrs(ifaddr);
+        return rb_if_hash;
 }
 
 VALUE mSystem;
